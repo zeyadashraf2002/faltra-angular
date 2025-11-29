d3735b6605a518e303ee85f2dfd4f19acdeeb782
zeyadashraf2002 <zezoashraf283@gmail.com>
2025-11-29 18:37:29 +0200

    fix env to production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://faltarah-backend.onrender.com/api',
+  APP_URL: 'https://faltarah.vercel.app/',
+  emailServiceId: 'service_jen8abg',
+  emailTemplateId: 'template_4640mct',
+  emailPublicKey: '4KjDrgUtqXb4RIpUZ',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// 📁 src/environments/environment.ts
+export const environment = {
+  production: false,
+  API_URL: 'http://localhost:3000/api',
+  APP_URL: 'http://localhost:5173',
+  emailServiceId: 'service_jen8abg',
+  emailTemplateId: 'template_4640mct',
+  emailPublicKey: '4KjDrgUtqXb4RIpUZ',
+};
+
+
