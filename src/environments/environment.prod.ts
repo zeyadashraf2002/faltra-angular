db9521b4b1fd92ba97b8f791e7300d4ae5568099
‪zeyad ashraf‬‏ <84470860+zeyadashraf2002@users.noreply.github.com>
2026-01-30 01:24:48 +0200

    change domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_URL: 'https://faltarah-backend.onrender.com/api',
-  APP_URL: 'https://faltarah.vercel.app/',
+  APP_URL: 'https://erp.faltrah.cloud/',
   emailServiceId: 'service_jen8abg',
   emailTemplateId: 'template_4640mct',
   emailPublicKey: '4KjDrgUtqXb4RIpUZ',
